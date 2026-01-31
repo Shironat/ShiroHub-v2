8a40377d27c38db9ab12e66fea10e4e7250e6624
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-01-31 20:24:28 -0300

    Criar Shared/Services.lua

---
+++ Shared/Services.lua
@@ -0,0 +1,11 @@
+local Services = {
+    Players = game:GetService("Players"),
+    Teams = game:GetService("Teams"),
+    UIS = game:GetService("UserInputService"),
+    RunService = game:GetService("RunService"),
+    VirtualUser = game:GetService("VirtualUser"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    TeleportService = game:GetService("TeleportService")
+}
+
+return Services
