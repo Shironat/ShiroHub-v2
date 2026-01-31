4bf0b455dfe3f782625dba7ca4e83084997bb7ec
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-01-31 20:26:43 -0300

    Criar PlayerUtils.lua

---
+++ Shared/PlayerUtils.lua
@@ -0,0 +1,28 @@
+local S = require(script.Parent.Services)
+local Players = S.Players
+local TeleportService = S.TeleportService
+
+local Utils = {}
+
+function Utils.Character(player)
+    return player.Character or player.CharacterAdded:Wait()
+end
+
+function Utils.Humanoid(player)
+    return Utils.Character(player):WaitForChild("Humanoid")
+end
+
+function Utils.HRP(player)
+    return Utils.Character(player):WaitForChild("HumanoidRootPart")
+end
+
+function Utils.ForceReset(player)
+    local hum = Utils.Humanoid(player)
+    hum.Health = 0
+end
+
+function Utils.Rejoin(player)
+    TeleportService:Teleport(game.PlaceId, player)
+end
+
+return Utils
