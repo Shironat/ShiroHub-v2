3f2de24a2f911b638f7f133e6029e7e208e715dd
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-01-31 20:27:43 -0300

    Criar States.lua

---
+++ Shared/States.lua
@@ -0,0 +1,22 @@
+local State = {
+    ESP = {
+        Enabled = true,
+        MaxDistance = 400,
+        Cache = {}
+    },
+
+    Movement = {
+        Speed = false,
+        Jump = false,
+        Fly = false,
+        Noclip = false
+    },
+
+    Loaded = {
+        Dex = false,
+        Spy = false,
+        Inf = false
+    }
+}
+
+return State
