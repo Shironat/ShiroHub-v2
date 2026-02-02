f2970cd8ee301f50b308f9280021d43a492964a6
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 14:26:25 -0300

    Atualizar o ShiroHub.lua

--- ShiroHub.lua
+++ ShiroHub.lua
@@ -1,29 +1,14 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
-local Window = Rayfield:CreateWindow({
-    Name = "ShiroHub v2"
-})
+local Window = Rayfield:CreateWindow({ Name = "ShiroHub v2" })
 
 local Tabs = {
     Exploits = Window:CreateTab("Exploits"),
     Inject   = Window:CreateTab("Injection"),
     Logs     = Window:CreateTab("Logs"),
-    District = Window:CreateTab("District"),
-    Tsunami  = Window:CreateTab("Tsunami")
 }
 
-local Root = script.Parent
-
-local Modules = {
-    Exploits = require(Root.Tabs.Exploits),
-    Inject   = require(Root.Tabs.Inject),
-    Logs     = require(Root.Tabs.Logs),
-    District = require(Root.Tabs.District),
-    Tsunami  = require(Root.Tabs.Tsunami)
-}
-
-for name, module in pairs(Modules) do
-    if module.Init then
-        module.Init(Tabs[name])
-    end
-end
+-- carrega módulos REMOTAMENTE
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Tabs/Exploits.lua"))()(Tabs.Exploits)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Tabs/Inject.lua"))()(Tabs.Inject)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Tabs/Logs.lua"))()(Tabs.Logs)
