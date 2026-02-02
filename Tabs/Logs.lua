4b074b09cdead9560a2f17ddb9cb1ad65c49aacc
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 18:14:59 -0300

    Atualizar o Logs.lua

--- Tabs/Logs.lua
+++ Tabs/Logs.lua
@@ -1,2 +1,22 @@
-print("carregando Logs...")
-print("Logs carregado")
+print("Logs.lua carregando")
+
+return function(Tab)
+    print("Logs inicializada")
+
+    -- Se precisar de lógica externa
+    -- local Logic = loadstring(game:HttpGet("LINK_DA_LOGICA.lua"))()
+
+    Tab:CreateButton({
+        Name = "Exemplo Botão2",
+        Callback = function()
+            print("Botão clicado")
+        end
+    })
+
+    Tab:CreateToggle({
+        Name = "Exemplo Toggle2",
+        Callback = function(state)
+            print("Toggle:", state)
+        end
+    })
+end
