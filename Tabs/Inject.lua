db4ffdde5d7da22a47ef4a373f95b451c12ec807
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 18:13:28 -0300

    Atualizar o Inject.lua

--- Tabs/Inject.lua
+++ Tabs/Inject.lua
@@ -1,1 +1,21 @@
-print("carregando Inject...")
+print("Inject.lua carregando...")
+
+return function(Tab)
+    print("Inject inicializada")
+
+    -- local Logic = loadstring(game:HttpGet("LINK_DA_LOGICA.lua"))()
+
+    Tab:CreateButton({
+        Name = "Exemplo Botão",
+        Callback = function()
+            print("Botão clicado")
+        end
+    })
+
+    Tab:CreateToggle({
+        Name = "Exemplo Toggle",
+        Callback = function(state)
+            print("Toggle:", state)
+        end
+    })
+end
