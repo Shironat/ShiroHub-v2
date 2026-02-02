f1777ca5cee89f350e3d980d7624df6da8aeddb0
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 15:56:48 -0300

    Atualizar o Inject.lua

--- Tabs/Inject.lua
+++ Tabs/Inject.lua
@@ -1,16 +1,2 @@
-local State = require(script.Parent.Parent.Shared.State)
-
-local Inject = {}
-
-function Inject.Init(Window, Tab)
-    Tab:CreateButton({
-        Name = "Infinite Yield",
-        Callback = function()
-            if State.Loaded.Inf then return end
-            State.Loaded.Inf = true
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-        end
-    })
-end
-
-return Inject
+print("carregando Inject...")
+print("Inject carregado")
