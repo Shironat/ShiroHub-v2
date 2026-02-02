406ac5c849e82685d480d4aa05a0ba05011abded
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 15:57:33 -0300

    Atualizar o District.lua

--- Tabs/District.lua
+++ Tabs/District.lua
@@ -1,23 +1,2 @@
-local S = require(script.Parent.Parent.Shared.Services)
-local State = require(script.Parent.Parent.Shared.State)
-
-local District = {}
-
-function District.Init(Window, Tab)
-    Tab:CreateToggle({
-        Name = "Ataque - District",
-        Callback = function(v)
-            State.Attacking = v
-            if v then
-                task.spawn(function()
-                    while State.Attacking do
-                        S.ReplicatedStorage.Remotes.Attacks.BasicAttack:FireServer()
-                        task.wait()
-                    end
-                end)
-            end
-        end
-    })
-end
-
-return District
+print("carregando District...")
+print("District Carregado")
