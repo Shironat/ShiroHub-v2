d1103d3f71fba61286cf7d33aba9bf9d014ecc20
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 15:12:10 -0300

    Atualizar o ShiroHub.lua

--- ShiroHub.lua
+++ ShiroHub.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({ Name = "ShiroHub v2" })
 
-print("Carregando Tabs)
+print("Carregando Tabs")
 local Tabs = {
     Exploits = Window:CreateTab("Exploits"),
     Inject   = Window:CreateTab("Injection"),
