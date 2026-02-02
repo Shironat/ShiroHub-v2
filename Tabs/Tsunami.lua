f1917445add1df7b283ef23b716527afd0fb6a74
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 19:00:06 -0300

    Atualizar o Tsunami.lua

--- Tabs/Tsunami.lua
+++ Tabs/Tsunami.lua
@@ -1,6 +1,6 @@
 return function(Tab)
     local Logic = loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Logic/LogicTsunami.lua"
+        "https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Logic/TsunamiLogic.lua"
     ))()
 
     Tab:CreateToggle({
