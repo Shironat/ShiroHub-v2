ab0eea8183b468343ec17b86c71133e501f6a11e
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-03 11:32:50 -0300

    Atualizar o Tsunami.lua

--- Tabs/Tsunami.lua
+++ Tabs/Tsunami.lua
@@ -3,35 +3,37 @@ return function(Tab)
         "https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Logic/TsunamiLogic.lua"
     ))()
 
-    local brainrots = {}
-    local selectedSlot = nil
-    local BrainrotDropdown = nil
-
-    Tab:CreateSection("Reset(If it stops working)")
-
     Tab:CreateButton({
         Name = "Reset Base",
-        Callback = function()
-            Logic.ResetBase()
-        end
+        Callback = Logic.ResetBase
     })
-
-    Tab:CreateSection("AutoFarms")
 
     Tab:CreateToggle({
         Name = "Auto Collect",
-        Callback = function(state)
-            Logic.ToggleMoney(state)
-        end
+        Callback = Logic.ToggleMoney
     })
 
-    Tab:CreateToggle({
-        Name = "Auto Event Coins",
-        Callback = function(state)
-            Logic.ToggleMoney(state)
+    local brainrots = Logic.GetBrainrots()
+
+    local options = {}
+    local selectedSlot = nil
+
+    for _, b in ipairs(brainrots) do
+        table.insert(options, b.Name)
+    end
+
+    Tab:CreateDropdown({
+        Name = "Selecionar Brainrot",
+        Options = options,
+        Callback = function(selected)
+            for _, b in ipairs(brainrots) do
+                if b.Name == selected then
+                    selectedSlot = b.Slot
+                    break
+                end
+            end
         end
     })
-
 
     Tab:CreateToggle({
         Name = "Auto Upgrade Brainrot",
@@ -39,30 +41,8 @@ return function(Tab)
             if selectedSlot then
                 Logic.ToggleUpgrade(state, selectedSlot)
             else
-                warn("[Tsunami] Nenhum Brainrot selecionado")
+                warn("[Tsunami] Nenhum slot selecionado")
             end
         end
-    })
-
-    Tab:CreateDropdown({
-    Name = "Selecionar Brainrot",
-    Options = options,
-    Callback = function(selected)
-        -- Rayfield pode retornar string OU tabela
-        local selectedName = selected
-        if type(selected) == "table" then
-            selectedName = selected.Value
-        end
-
-        selectedSlot = nil
-
-        for _, b in ipairs(brainrots) do
-            if b.Name == selectedName then
-                selectedSlot = b.Slot
-                print("[Tsunami] Slot selecionado:", selectedSlot, b.Name)
-                break
-            end
-        end
-    end
     })
 end
