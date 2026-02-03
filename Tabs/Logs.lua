c040917ff7ef6676a98cc71f42eb57b41accf523
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 21:10:31 -0300

    Excluir Logs.lua

--- Tabs/Logs.lua
+++
@@ -1,5 +0,0 @@
-local Logic = loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Logic/LogsLogic.lua"))()
-
-local Logs = Logic(Tab)
-Logic.PrintLine("ShiroHub Logs iniciado!")
-end
