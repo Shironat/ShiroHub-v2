54eda16a6483e88ac14a5be6e6fb677fe57cd5a4
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-03 00:14:40 -0300

    Atualizar o Tsunami.lua

--- Tabs/Tsunami.lua
+++ Tabs/Tsunami.lua
@@ -3,28 +3,27 @@ return function(Tab)
         "https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Logic/TsunamiLogic.lua"
     ))()
 
-Tab:CreateToggle({
-    Name = "Reset Base",
-    Callback = function(_)
-        -- O underscore ignora qualquer argumento passado pelo toggle
-        if Logic and Logic.ResetBase then
-            pcall(Logic.ResetBase)
-        else
-            warn("Logic.ResetBase não existe!")
+    Tab:CreateToggle({
+        Name = "Reset Base",
+        Callback = function(_)
+           if Logic and Logic.ResetBase then
+               pcall(Logic.ResetBase)
+           else
+               warn("Logic.ResetBase = nil")
+           end
         end
-    end
-})
+    })
 
-Tab:CreateToggle({
-    Name = "Auto Collect",
-    Callback = function(enabled)
-        if Logic and Logic.ToggleMoney then
-            pcall(function()
-                Logic.ToggleMoney(enabled)
-            end)
-        else
-            warn("Logic.ToggleMoney não existe!")
+    Tab:CreateToggle({
+        Name = "Auto Collect",
+        Callback = function(enabled)
+           if Logic and Logic.ToggleMoney then
+               pcall(function()
+               Logic.ToggleMoney(enabled)
+              end)
+           else
+              warn("Logic.ToggleMoney = nil")
+           end
         end
-    end
-})
+    })
 end
