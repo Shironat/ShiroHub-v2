582bd389bca09c76a05b30a44523ab4c21bfe329
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-02 21:11:39 -0300

    Atualizar o ShiroHub.lua

--- ShiroHub.lua
+++ ShiroHub.lua
@@ -9,7 +9,6 @@ local Tabs = {}
 
 Tabs.Exploits = Window:CreateTab("Exploits")
 Tabs.Inject   = Window:CreateTab("Injection")
-Tabs.Logs     = Window:CreateTab("Logs")
 Tabs.Tsunami  = Window:CreateTab("Tsunami")
 
 assert(Tabs.Tsunami, "Tab Tsunami não existe")
@@ -19,8 +18,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/
 print("EXPLOITS: 200[OK]")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Tabs/Inject.lua"))()(Tabs.Inject)
 print("INJECT: 200[OK]")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Tabs/Logs.lua"))()(Tabs.Logs)
-print("LOGS: 200[OK]")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Tabs/Tsunami.lua"))()(Tabs.Tsunami)
 print("TSUNAMI: 200[OK]")
 
