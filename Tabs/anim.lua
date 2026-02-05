ac32616b7bd3db13aa3c9ea1951f6cfe448e200c
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-05 20:25:28 -0300

    Atualizar o anim.lua

--- Tabs/anim.lua
+++ Tabs/anim.lua
@@ -1,15 +1,15 @@
 return function(Tab)
 
-local Logic =         loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Logic/TsunamiLogic.lua"))()
+    local Logic = loadstring(game:HttpGet("https://raw.githubusercontent.com/Shironat/ShiroHub-v2/main/Logic/TsunamiLogic.lua"))()
 
-Tab:CreateToggle({
-    Name = "Auto TP",
-    CurrentValue = false,
-    Callback = function(state)
-        if state then
-            Logic.Start()
-        else
-            Logic.Stop()
+    Tab:CreateToggle({
+        Name = "Auto TP",
+        CurrentValue = false,
+        Callback = function(state)
+            if state then
+                Logic.Start()
+            else
+                Logic.Stop()
+            end
         end
-    end
-})
+        })
